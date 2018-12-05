6659f056a530d66f2ea297120e8c41841b4da482
Kiziltoprak Zafer <Kiziltoprak.Zafer@platinion.com>
2018-12-05 23:28:43 +0100

    add the base classes for Day and Puzzle

---
+++ src/DayFactory.py
@@ -0,0 +1,16 @@
+# src/DayFactory.py
+
+from src.Days.Day1 import Day1
+
+advent_days = {
+    'Day1': Day1()
+}
+
+
+class DayFactory:
+    """
+    Day Factory class
+    """
+    @staticmethod
+    def instantiate(day_number=0):
+        return advent_days["Day" + str(day_number)]
